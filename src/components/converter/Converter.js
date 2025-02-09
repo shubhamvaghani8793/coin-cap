46c6f7eeb96d7896a57198b4e6e679b8eefc40fc
Dhruv-1511 <dhruvsheladiya07@gmail.com>
2025-02-09 14:14:31 +0530

    Converter page complete

--- src/components/converter/Converter.js
+++ src/components/converter/Converter.js
@@ -1,45 +1,66 @@
 import React from 'react'
 import ContactUs from '../contactus/ContactUs'
+import Navbar from '../navbar/Navbar'
+import { FaAngleDown } from "react-icons/fa6";
 
 const Converter = () => {
     return (
         <>
-            <div className='relative place-items-center'>
-                    <img src='converterbg1.png' alt='' className='absolute top-0 left-0 h' />
-                    <img src='converterbg2.png' alt='' className='absolute top-0 right-0 h-[800px]' />
-                <div className='max-w-4xl w-full '>
-                    <p className='text-[75px] text-white text-center font-semibold max-w-[800px] w-full'>Crypto & <span className='bg-gradient-to-r from-[#F3510D] to-[#F67611] bg-clip-text text-transparent'>Currency</span> Converter</p>
-                    <div className="border border-[#676767] rounded-xl py-6 px-6 mt-10 bg-transparent">
+            <Navbar />
+            <img src='converterbg1.png' alt='' className='absolute top-0 left-0 h-80 md:h-auto' />
+            <img src='converterbg2.png' alt='' className='absolute top-52 right-0  w-[350px] h-[700px] md:top-0 md:h-[800px] md:w-auto' />
+            <div className='relative place-items-center mt-5 mx-5 md:mx-0 md:mt-20'>
+                <div className='max-w-4xl w-full place-items-center'>
+                    <p className='text-[30px] text-white text-center font-semibold max-w-[300px] w-full sm:max-w-[500px] sm:text-[40px] md:text-[75px] md:max-w-[800px]'>Crypto & <span className='bg-gradient-to-r from-[#F3510D] to-[#F67611] bg-clip-text text-transparent'>Currency</span> Converter</p>
+                    <div className="border border-[#676767] w-full md:w-auto rounded-xl mx-2 py-6 px-4 mt-10 bg-transparent sm:px-6">
                         <div className='place-items-center'>
                             {/* Dropdown */}
-                            <div className="mb-4 flex justify-center">
-                                <select className="bg-[#23232E] text-white p-4 rounded-md w-72 ">
+                            <div className="relative mb-4 flex justify-center w-full md:w-auto">
+                                <select className="bg-[#23232E] outline-none appearance-none text-white p-4 rounded-md w-full md:w-72 ">
                                     <option>Crypto to Crypto</option>
                                 </select>
+
+                                <div className="absolute inset-y-0 right-3 flex items-center pointer-events-none">
+                                    <FaAngleDown />
+                                </div>
                             </div>
 
                             {/* Crypto Selection */}
-                            <div className="mt-16 flex items-center">
+                            <div className="mt-16 flex flex-col items-center md:flex-row gap-3 md:gap-0">
                                 {/* Ethereum Box */}
-                                <div className="flex items-center bg-[#23232E] px-4 py-2 rounded-md gap-2">
-                                    <img src="ethereum.svg" alt="Ethereum" className="h-5 w-5" />
-                                    <select className="bg-transparent outline-none w-48 text-white cursor-pointer">
-                                        <option className='bg-[#23232e]'>Ethereum</option>
-                                    </select>
-                                    <p className='text-white bg-[#383848] rounded-lg px-4 py-2 ml-3'>$3.600</p>
+                                <div className="flex items-center bg-[#23232E] w-full justify-between px-4 py-2 rounded-md gap-2">
+                                    <div className='flex gap-2 items-center w-[125px] md:w-[225px] sm:w-auto'>
+
+                                        <img src="ethereum.svg" alt="Ethereum" className="h-5 w-5" />
+                                        <select className="bg-transparent outline-none w-24 text-white cursor-pointer sm:w-48">
+                                            <option style={{ backgroundColor: "#23232E", color: "white" }} value="usd">
+                                                Ethereum
+                                            </option>
+                                            <option style={{ backgroundColor: "#23232E", color: "white" }} value="usd">
+                                                Ethereum
+                                            </option>
+                                        </select>
+                                    </div>
+                                    <input className='text-white bg-[#383848] rounded-lg w-20 py-2 pl-4 sm:w-28' placeholder='$3.600' />
                                 </div>
 
                                 {/* Swap Icon */}
-                                <img src="leftright.svg" alt="Ethereum" className="h-5 w-5 mx-5" />
+                                <img src="leftright.svg" alt="Ethereum" className="h-5 w-5 mx-5 rotate-90 md:rotate-0" />
 
                                 {/* Bitcoin Box */}
-                                <div className="flex items-center bg-[#23232E] px-4 py-2 rounded-md gap-2">
-                                    <img src="bitcoin.svg" alt="Ethereum" className="h-5 w-5" />
-                                    <select className="bg-transparent outline-none w-48 text-white cursor-pointer">
-                                        <option className='bg-transparent'>Bitcoin</option>
-
-                                    </select>
-                                    <p className='text-white bg-[#383848] rounded-lg px-4 py-2 ml-3'>$3.600</p>
+                                <div className="flex items-center bg-[#23232E] w-full justify-between px-4 py-2 rounded-md gap-2">
+                                    <div className='flex gap-2 items-center w-[125px] md:w-[225px] sm:w-auto'>
+                                        <img src="bitcoin.svg" alt="Ethereum" className="h-5 w-5" />
+                                        <select className="bg-transparent outline-none w-24 text-white cursor-pointer sm:w-48">
+                                            <option style={{ backgroundColor: "#23232E", color: "white" }} value="usd">
+                                                Bitcoin
+                                            </option>
+                                            <option style={{ backgroundColor: "#23232E", color: "white" }} value="usd">
+                                                Bitcoin
+                                            </option>
+                                        </select>
+                                    </div>
+                                    <p className='text-white py-2 '>$3.600</p>
                                 </div>
                             </div>
                         </div>
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,49 +3,51 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <div className="bg-[#10101A] text-white py-8 pl-6 pr-20 flex items-start">
-      <img src='logo.svg' alt='' />
-      <div className='w-full'>
-        <div className="mt-2 mx-auto flex justify-between">
-          <div className='max-w-60'>
+    <div className="bg-[#10101A] text-white py-8 pl-2 pr-2 flex items-start lg:pl-6 lg:pr-20">
+      <img src='logo.svg' alt='' className='hidden md:flex' />
+      <div className='w-full '>
+        <div className="mt-3 mx-auto flex flex-col gap-6 justify-between items-center text-center md:items-start md:gap-2 md:text-start md:flex-row">
+          <div className='w-full md:w-60 place-items-center'>
             <h2 className="text-orange-500 text-2xl font-bold flex items-center gap-2">
+            <img src='logo.svg' alt='' className='md:hidden' />
+
               Coin Cap Converter
             </h2>
-            <p className="text-lg mt-6">
+            <p className="mt-3 max-w-[200px] pl-2 md:pl-0 md:w-auto md:mt-6 md:text-lg">
               Convert Crypto, Calculate Market Caps, and Track Projections.
             </p>
           </div>
 
           <div>
             <h3 className="text-white text-2xl font-bold">Quick Link</h3>
-            <ul className="mt-6 text-sm ">
-              <li><a href="/" className="hover:text-orange-500 text-[18px]">Contact Us</a></li>
+            <ul className="mt-3 text-xs md:mt-6 md:text-sm">
+              <li><a href="/" className="hover:text-orange-500 md:text-[18px]">Contact Us</a></li>
             </ul>
           </div>
 
           <div>
             <h3 className="text-white text-2xl font-bold">Help</h3>
-            <ul className="mt-6 text-sm flex flex-col gap-4">
-              <li><a href="/" className=" hover:text-orange-500 text-[18px]">Cookies Policy</a></li>
-              <li><a href="/" className="hover:text-orange-500 text-[18px]">Terms & Conditions</a></li>
-              <li><a href="/" className="hover:text-orange-500 text-[18px]">Privacy Policy</a></li>
+            <ul className="mt-3 text-xs md:mt-6 md:text-sm md:gap-4 flex flex-col gap-2">
+              <li><a href="/" className=" hover:text-orange-500 md:text-[18px]">Cookies Policy</a></li>
+              <li><a href="/" className="hover:text-orange-500 md:text-[18px]">Terms & Conditions</a></li>
+              <li><a href="/" className="hover:text-orange-500 md:text-[18px]">Privacy Policy</a></li>
             </ul>
           </div>
 
           <div>
             <h3 className="text-white text-2xl font-bold">Download App</h3>
-            <div className='mt-6 flex flex-col gap-2'>
+            <div className='mt-3 md:mt-6 flex flex-col gap-2'>
 
-              <div className='w-[181px] p-3 rounded-xl flex items-center gap-5 bg-white'>
-                <img src="playstore.svg" alt="Google Play" className='h-9 w-9' />
-                <div className=''>
+              <div className='w-[181px] p-3 rounded-xl flex items-center gap-5 bg-white py-2 md:py-3'>
+                <img src="playstore.svg" alt="Google Play" className='h-7 w-7 md:h-9 md:w-9' />
+                <div className='text-start'>
                   <p className='text-black text-[10px] font-semibold'>Get it On</p>
                   <p className='text-black text-lg font-semibold'>Google Play</p>
                 </div>
               </div>
-              <div className=' p-3 rounded-xl flex items-center gap-5 bg-white'>
-                <img src="appstore.svg" alt="Google Play" className='h-9 w-9' />
-                <div className=''>
+              <div className='w-[181px] p-3 rounded-xl flex items-center gap-5 bg-white py-2 md:py-3'>
+                <img src="appstore.svg" alt="Google Play" className='h-7 w-7 md:h-9 md:w-9' />
+                <div className='text-start'>
                   <p className='text-black text-[10px] font-semibold'>Download On The</p>
                   <p className='text-black text-xl font-semibold'>App Store</p>
                 </div>
@@ -54,12 +56,12 @@ function Footer() {
           </div>
         </div>
         <hr className='mt-10' />
-        <div className='flex items-center justify-between mt-4'>
+        <div className='flex items-center justify-between mt-4 flex-col sm:flex-row'>
           <p className="text-center text-gray-500 text-sm">
             © Copyright 2025, all right reserve by
           </p>
 
-          <div className="flex justify-center gap-4">
+          <div className="flex justify-center gap-4 mt-5 sm:mt-0">
             <Link to="https://x.com/Coincapconvert"><img src='x.svg' alt='' /></Link>
             <Link to=""><img src='tiktok.svg' alt='' /></Link>
             <Link to="https://www.instagram.com/coincapconvert/"><img src='instagram.svg' alt='' /></Link>
