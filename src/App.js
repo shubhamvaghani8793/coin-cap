b552bcc91ac2cdd3737db4367ccc51201835c7c8
Dhruv-1511 <dhruvsheladiya07@gmail.com>
2025-02-09 11:47:26 +0530

    Contact us responsive

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/footer/Footer'
 import Home from './components/home/Home';
 import Converter from './components/converter/Converter';
-import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/converter' element={<Converter />} />
